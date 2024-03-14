7f85de14f8413a35db78e0769502e8be9ce70c1e
Thomas Anderson <thomas@heythomasj.com>
2024-03-13 23:59:50 -0700

    Update some boilerplate for express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, {Application} from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+
+const app: Application = express();
+
+app.get('/', async (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error(error);
+}
